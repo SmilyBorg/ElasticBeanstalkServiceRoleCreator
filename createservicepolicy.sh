2fe8cba0f44b89d87e683493a0a5756230df532c
Una Alexia Karlsen <una@strangeminds.org>
2015-08-22 12:46:21 +0200

    fixed typo

--- createservicepolicy.sh
+++ createservicepolicy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROLE=$(aws iam create-role --role-name ElasticBeanstalk-Service-Role --assume-role-policy-document file://trustrelationship.json --output text)
-POLICY=$(aws create-policy --policy-name ElasticBeanstalk-Service-Role-Policy --policy-document file://servicerolepolicy.json --output text)
+POLICY=$(aws iam create-policy --policy-name ElasticBeanstalk-Service-Role-Policy --policy-document file://servicerolepolicy.json --output text)
 
-aws iam attach-role-policy --role-name ElasticBeanstalk-Service-Role --policy-arn  $(echo $POLICY | awk '{print$2}')
+aws iam attach-role-policy --role-name ElasticBeanstalk-Service-Role --policy-arn  $(echo $POLICY | awk '{print $2}')
+
