890a02f44d4eab7fae7a9b7dd1fe8f8f01af33b9
Una Alexia Karlsen <una@strangeminds.org>
2015-08-22 12:30:53 +0200

    got the bash script working and updated README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ElasticBeanstalkServiceRoleCreator
-Create the necessary service role for Elastic Beanstalk automatically
+Create the necessary service role for Elastic Beanstalk automatically as descrived at http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/services-iam-serviceroles.html
--- createservicepolicy.sh
+++ createservicepolicy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROLE=$(aws iam create-role --role-name ElasticBeanstalk-Service-Role --assume-role-policy-document file://trustrelationship.json)
-POLICY=$(aws create-policy --policy-name ElasticBeanstalk-Service-Role-Policy --policy-document file://servicerolepolicy.json)
+ROLE=$(aws iam create-role --role-name ElasticBeanstalk-Service-Role --assume-role-policy-document file://trustrelationship.json --output text)
+POLICY=$(aws create-policy --policy-name ElasticBeanstalk-Service-Role-Policy --policy-document file://servicerolepolicy.json --output text)
 
-//aws iam attach-role-policy --role-name ElasticBeanstalk-Service-Role --policy-arn arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole
+aws iam attach-role-policy --role-name ElasticBeanstalk-Service-Role --policy-arn  $(echo $POLICY | awk '{print$2}')
